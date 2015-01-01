20eb83f7c1dd9e6bacd4708cc4b2e9df9b5c0a9b
Nicholas Cole <nicholasjcole1@gmail.com>
2015-01-01 15:28:58 -0600

    Made labelling less manual, cleaned up formatting

--- make_map.r
+++ make_map.r
@@ -1,21 +1,15 @@
 # make map of GPS coords
 library(mapdata)
-
 setwd("/home/nicholas/Documents/LocationHistory")
-
-
 
 #get gps data
 gps_coords = read.csv('gps_total_rounded_to_three.csv', header = FALSE)
 
-
 # make map
-
 map("state", interior = FALSE)
 map("state", boundary = FALSE, col="gray", add = TRUE)
 
-
-
+# add GPS coords
 for (indexnum in c(1:length(gps_coords$V1))){
     points(gps_coords$V1[indexnum], gps_coords$V2[indexnum], col ="steelblue2", type = "p", pch = 19, cex = .75)
     print(indexnum)
@@ -24,15 +18,9 @@ for (indexnum in c(1:length(gps_coords$V1))){
 ###### Put title and legend ######
 title(main = "Travel in 2014")
 
-
 #### How to draw lines between ####
 #### http://flowingdata.com/2011/05/11/how-to-map-connections-with-great-circles/
 #### http://stackoverflow.com/questions/22282713/r-maps-package-how-to-draw-lines-between-cities-within-the-country
-
-
-### Code for plotting labels
-### text(-96.7967, 32.7758, labels = "test label", pos = 4, offset = TRUE, cex =.5)
-# plot points
 
 ### coordinates to label
 lasv <- c(-115.1739, 36.1215) #, "Las Vegas, NV")
@@ -44,27 +32,24 @@ austin <-c(-97.7500, 30.2500) #, "Austin, TX")
 bandera <- c(-99.0750, 29.7258)
 amory <- c(-88.4839, 33.9872)
 
-#cities_data <- data.frame(lasv, dallas, denver, sanf, dc, austin)
+cities_data <- data.frame(lasv, dallas, denver, sanf, dc, austin, bandera, amory)
+cities_names <- c("Las Vegas, NV",
+                  "Dallas, TX",
+                  "Denver, CO",
+                  "San Francisco, CA",
+                  "Washington, D.C.",
+                  "Austin, TX",
+                  "Bandera, TX",
+                  "Amory, MS")
+cities_label_pos <- c(4,2,3,4,3,4,1,1) # positions: 1=under, 2=left, 3=top, 4=right
 
-#for (item in cities_data){
-#  points(as.character(item[1]), as.character(item[2]))
-#  text(as.double(item[1]), as.double(item[2]), item[3], pos = 3, cex = 0.75, col = 'red')
-#  }
+### make points on map
+for (i in cities_data){
+  points(i[1], i[2], pch = 19, cex = .75, col = 'red')
+}
 
-# label cities, this is so manual it's embarrassing but had troubles with for loop/data types
-points(lasv[1], lasv[2], pch = 19, cex = .75, col = 'red')
-text(lasv[1], lasv[2], "Las Vegas, NV", pos = 4, cex = 0.75, col = 'red')
-points(dallas[1], dallas[2], pch = 19, cex = .75, col = 'red')
-text(dallas[1], dallas[2], "Dallas, TX", pos = 2, cex = 0.75, col = 'red')
-points(denver[1], denver[2], pch = 19, cex = .75, col = 'red')
-text(denver[1], denver[2], "Denver, CO", pos = 3, cex = 0.75, col = 'red')
-points(sanf[1], sanf[2], pch = 19, cex = .75, col = 'red')
-text(sanf[1], sanf[2], "San Francisco, CA", pos = 4, cex = 0.75, col = 'red')
-points(dc[1], dc[2], pch = 19, cex = .75, col = 'red')
-text(dc[1], dc[2], "Washington, D.C.", pos = 3, cex = 0.75, col = 'red')
-points(austin[1], austin[2], pch = 19, cex = .75, col = 'red')
-text(austin[1], austin[2], "Austin, TX", pos = 4, cex = 0.75, col = 'red')
-points(bandera[1], bandera[2], pch = 19, cex = .75, col = 'red')
-text(bandera[1], bandera[2], "Bandera, TX", pos = 1, cex = 0.75, col = 'red')
-points(amory[1], amory[2], pch = 19, cex = .75, col = 'red')
-text(amory[1], amory[2], "Amory, MS", pos = 1, cex = 0.75, col = 'red')
+# label points
+for (g in c(1:length(cities_names))){
+  text(cities_data[,g][1], cities_data[,g][2], cities_names[g], col = 'red', pos = cities_label_pos[g], cex = .75)
+}
+
